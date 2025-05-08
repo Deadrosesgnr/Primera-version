c6f2a8a28874635389d64d342344fd09a4eaef45
Rose <107224216+Deadrosesgnr@users.noreply.github.com>
2025-05-08 16:12:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const map = L.map('map').setView([-33.6151, -70.5754], 14); // Puente Alto
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap',
-}).addTo(map);
+}).addTo(map); //Muestra de calles
 
 const marker = L.marker([-33.6151, -70.5754]).addTo(map);
-marker.bindPopup('Carro de Bomberos').openPopup();
+marker.bindPopup('Carro de Bomberos').openPopup(); //Marcador
 
-// Simulación de movimiento
+// Simulación de movimiento en diagonal
 let lat = -33.6151;
 let lng = -70.5754;
 
